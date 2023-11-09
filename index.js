6f8cdc3296aa0767f9ce8aa18861f88b93f07dae
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2023-11-09 17:52:05 +0100

    Added eventlisteners for numbers and clear buttons

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
-let firstNumber
-let operator
-let secondNumber
+let firstNumber;
+let operator;
+let secondNumber;
+let displayValue = '';
 
+// All math functions
 function add(a, b) {
     return a + b;
 }
@@ -20,13 +22,90 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
-    if (operator === '+'){
+    if (operator === '+') {
         return add(a, b);
-    } else if (operator === '-'){
+    } else if (operator === '-') {
         return substract(a, b);
-    } else if (operator === '*'){
+    } else if (operator === '*') {
         return multiply(a, b);
-    } else if (operator === '/'){
+    } else if (operator === '/') {
         return divide(a, b);
     }
- }
+}
+
+// Append numbers functions
+function appendNumber(number) {
+    displayValue += number;
+}
+const number0 = document.getElementById('digit-0');
+number0.addEventListener('click', () => {
+    appendNumber('0');
+    updateDisplay();
+})
+
+const number1 = document.getElementById('digit-1');
+number1.addEventListener('click', () => {
+    appendNumber('1');
+    updateDisplay();
+})
+
+const number2 = document.getElementById('digit-2');
+number2.addEventListener('click', () => {
+    appendNumber('2');
+    updateDisplay();
+})
+
+const number3 = document.getElementById('digit-3');
+number3.addEventListener('click', () => {
+    appendNumber('3');
+    updateDisplay();
+})
+
+const number4 = document.getElementById('digit-4');
+number4.addEventListener('click', () => {
+    appendNumber('4');
+    updateDisplay();
+})
+
+const number5 = document.getElementById('digit-5');
+number5.addEventListener('click', () => {
+    appendNumber('5');
+    updateDisplay();
+})
+
+const number6 = document.getElementById('digit-6');
+number6.addEventListener('click', () => {
+    appendNumber('6');
+    updateDisplay();
+})
+
+const number7 = document.getElementById('digit-7');
+number7.addEventListener('click', () => {
+    appendNumber('07');
+    updateDisplay();
+})
+
+const number8 = document.getElementById('digit-8');
+number8.addEventListener('click', () => {
+    appendNumber('8');
+    updateDisplay();
+})
+
+const number9 = document.getElementById('digit-9');
+number9.addEventListener('click', () => {
+    appendNumber('9');
+    updateDisplay();
+})
+
+function updateDisplay(){
+    const displayElement = document.getElementById('screen');
+    displayElement.textContent = displayValue;
+}
+
+function clearDisplay(){
+    displayValue = '';
+    updateDisplay();
+}
+
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', clearDisplay)
